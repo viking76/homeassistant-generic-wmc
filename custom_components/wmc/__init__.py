d2648a699f5b2e9cb7280f1caab0651be4cb2da7
Gaël <liaudetgael@gmail.com>
2024-08-06 22:20:49 +0200

    correct

--- custom_components/wmc/__init__.py
+++ custom_components/wmc/__init__.py
@@ -1,97 +1,64 @@
-"""The generic_dew_point_wmc component."""
+"""The wmc component."""
 
 import voluptuous as vol
-
-from homeassistant.components.wmc import DOMAIN as WMC_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_NAME, CONF_UNIQUE_ID, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv, discovery
-from homeassistant.helpers.device import (
-    async_remove_stale_devices_links_keep_entity_device,
-)
+from homeassistant.helpers.device import async_remove_stale_devices_links_keep_entity_device
 from homeassistant.helpers.typing import ConfigType
 
 DOMAIN = "wmc"
 
-CONF_SENSOR_INDOOR_TEMP = "sensor_indoor_temp"
-CONF_SENSOR_INDOOR_HUMIDITY = "sensor_indoor_humidity"
-CONF_SENSOR_OUTDOOR_TEMP = "sensor_outdoor_temp"
-CONF_SENSOR_OUTDOOR_HUMIDITY = "sensor_outdoor_humidity"
-CONF_DELTA_TRIGGER = "delta_trigger"
-CONF_TARGET_OFFSET = "target_offset"
-CONF_MIN_ON_TIME = "min_on_time"
-CONF_MAX_ON_TIME = "max_on_time"
-CONF_MIN_HUMIDITY = "min_humidity"
-CONF_SAMPLE_INTERVAL = "sample_interval"
-CONF_LOW_SPEED = "low_speed"
-CONF_HIGH_SPEED = "high_speed"
-
-DEFAULT_TOLERANCE = 3
-DEFAULT_NAME = "Generic Dew Point WMC"
-
-WMC_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_SENSOR_INDOOR_TEMP): cv.entity_id,
-        vol.Required(CONF_SENSOR_INDOOR_HUMIDITY): cv.entity_id,
-        vol.Required(CONF_SENSOR_OUTDOOR_TEMP): cv.entity_id,
-        vol.Required(CONF_SENSOR_OUTDOOR_HUMIDITY): cv.entity_id,
-        vol.Required(CONF_LOW_SPEED): cv.entity_id,
-        vol.Required(CONF_HIGH_SPEED): cv.entity_id,
-        vol.Optional(CONF_DELTA_TRIGGER, default=DEFAULT_TOLERANCE): vol.Coerce(float),
-        vol.Optional(CONF_TARGET_OFFSET, default=DEFAULT_TOLERANCE): vol.Coerce(float),
-        vol.Optional(CONF_MIN_ON_TIME): vol.All(cv.time_period, cv.positive_timedelta),
-        vol.Optional(CONF_MAX_ON_TIME): vol.All(cv.time_period, cv.positive_timedelta),
-        vol.Optional(CONF_MIN_HUMIDITY): vol.Coerce(float),
-        vol.Optional(CONF_SAMPLE_INTERVAL): vol.All(cv.time_period, cv.positive_timedelta),
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_UNIQUE_ID): cv.string,
-    }
-)
-
 CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: vol.All(cv.ensure_list, [WMC_SCHEMA])},
+    {DOMAIN: vol.All(cv.ensure_list, [vol.Schema({
+        vol.Required("name"): cv.string,
+        vol.Required("sensor_indoor_temp"): cv.entity_id,
+        vol.Required("sensor_indoor_humidity"): cv.entity_id,
+        vol.Required("sensor_outdoor_temp"): cv.entity_id,
+        vol.Required("sensor_outdoor_humidity"): cv.entity_id,
+        vol.Optional("delta_trigger", default=3): vol.Coerce(float),
+        vol.Optional("target_offset", default=3): vol.Coerce(float),
+        vol.Optional("min_on_time", default="00:00:00"): cv.time_period,
+        vol.Optional("max_on_time", default="02:00:00"): cv.time_period,
+        vol.Optional("sample_interval", default="00:05:00"): cv.time_period,
+        vol.Optional("min_humidity", default=0): vol.Coerce(float),
+        vol.Optional("unique_id"): cv.string,
+    })},
     extra=vol.ALLOW_EXTRA,
 )
 
-
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Generic Dew Point WMC component."""
+    """Set up the WMC component."""
     if DOMAIN not in config:
         return True
 
     for wmc_conf in config[DOMAIN]:
         hass.async_create_task(
             discovery.async_load_platform(
-                hass, Platform.WMC, DOMAIN, wmc_conf, config
+                hass, "sensor", DOMAIN, wmc_conf, config
             )
         )
 
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from a config entry."""
-
     async_remove_stale_devices_links_keep_entity_device(
         hass,
         entry.entry_id,
-        entry.options[CONF_SENSOR_INDOOR_TEMP],
+        entry.options["sensor_indoor_temp"],
     )
 
-    await hass.config_entries.async_forward_entry_setups(entry, (Platform.WMC,))
+    await hass.config_entries.async_forward_entry_setups(entry, ("sensor",))
     entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
     return True
-
 
 async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Update listener, called when the config entry options are changed."""
     await hass.config_entries.async_reload(entry.entry_id)
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(
-        entry, (Platform.WMC,)
+        entry, ("sensor",)
     )
-
